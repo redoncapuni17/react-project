0a600333c0f59c652149569be65645c01afc683b
redon <redon@gmail.com>
2023-03-27 11:36:00 +0300

    Third Commit (button)

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React,{useState} from "react";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpanseDate from "./ExpenseDate";
 
+
 function ExpenseItem(props) {
+
+  const [title,setTitle] = useState(props.title);
+  const [counter,setCounter]=useState(0);
+
+
+  const clickHandler = () =>{
+  setTitle("Update");
+  console.log("Title changed:"+title);
+
+  };
+  const clickCounterHandler = () =>{
+    setCounter(counter+1);
+    console.log("Title changed:"+title);
+  
+    };
    
   return (
     <Card className="expense-item">
       <ExpanseDate date ={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={clickCounterHandler}>{counter}</button>
     </Card>
   );
 }
